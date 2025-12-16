b82b53ae2ce4180ac4a65275f1ea8161df2b4179
jafega <45241209+jafega@users.noreply.github.com>
2025-12-16 15:48:03 +0100

    solve css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+PS C:\Users\garry\Downloads\dygo> npm -v
+11.6.2
+PS C:\Users\garry\Downloads\dygo> npm install -D tailwindcss postcss autoprefixer
+
+added 4 packages, and audited 245 packages in 1s
+
+54 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\garry\Downloads\dygo> npx tailwindcss init -p      
+npm error could not determine executable to run
+npm error A complete log of this run can be found in: C:\Users\garry\AppData\Local\npm-cache\_logs\2025-12-16T14_44_23_839Z-debug-0.log
+PS C:\Users\garry\Downloads\dygo> ^C
+PS C:\Users\garry\Downloads\dygo> npx tailwindcss init -p
+>>
+npm error could not determine executable to run
+npm error A complete log of this run can be found in: C:\Users\garry\AppData\Local\npm-cache\_logs\2025-12-16T14_44_45_144Z-debug-0.log
+PS C:\Users\garry\Downloads\dygo> 
