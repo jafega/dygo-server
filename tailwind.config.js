51158b77772ed3a29a2e6c02d6d72c3c35168963
jafega <45241209+jafega@users.noreply.github.com>
2025-12-16 16:10:19 +0100

    fix ux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
-PS C:\Users\garry\Downloads\dygo> npm -v
-11.6.2
-PS C:\Users\garry\Downloads\dygo> npm install -D tailwindcss postcss autoprefixer
-
-added 4 packages, and audited 245 packages in 1s
-
-54 packages are looking for funding
-  run `npm fund` for details
-
-found 0 vulnerabilities
-PS C:\Users\garry\Downloads\dygo> npx tailwindcss init -p      
-npm error could not determine executable to run
-npm error A complete log of this run can be found in: C:\Users\garry\AppData\Local\npm-cache\_logs\2025-12-16T14_44_23_839Z-debug-0.log
-PS C:\Users\garry\Downloads\dygo> ^C
-PS C:\Users\garry\Downloads\dygo> npx tailwindcss init -p
->>
-npm error could not determine executable to run
-npm error A complete log of this run can be found in: C:\Users\garry\AppData\Local\npm-cache\_logs\2025-12-16T14_44_45_144Z-debug-0.log
-PS C:\Users\garry\Downloads\dygo> 
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./App.tsx",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
